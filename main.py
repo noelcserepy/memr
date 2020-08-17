0072ebe4ac48fd9e679d2ded5e28ad637b6b9874
noelcserepy <noelcserepy@gmail.com>
2020-08-17 11:37:23 +0200

    Added pipenv. Added allmemes command. Added MongoDB. Fixed YouTube downloading issue. Some refactoring

--- main.py
+++ main.py
@@ -9,9 +9,16 @@ from datetime import datetime
 from pytube import YouTube
 from discord.ext import commands
 from tinydb import TinyDB, Query
+import pymongo
+from pymongo import MongoClient
 
+# Establishing a connection to the DB
+mongoToken = os.getenv("MONGO_TOKEN")
+cluster = MongoClient(mongoToken)
+db = cluster["Memr"]
 
-token = os.getenv("DISCORD_BOT_TOKEN")
+# Establishing a connection to the Discord API
+discordToken = os.getenv("DISCORD_BOT_TOKEN")
 client = commands.Bot(command_prefix='$')
 
 
@@ -20,22 +27,43 @@ async def on_ready():
     print(f"Logged in as {client}")
 
 
+# Bot joins current voice channel
 @client.command()
 async def join(ctx):
     senderVoiceChannel = ctx.message.author.voice.channel
     await senderVoiceChannel.connect(timeout=30.0, reconnect=True)
 
 
+# Bot leaves current voice channel
 @client.command()
 async def leave(ctx):
     await ctx.voice_client.disconnect()
 
 
+# Bot echoes any argument
 @client.command()
 async def test(ctx, arg):
     await ctx.send(arg)
 
 
+# Bot returns a list of all added memes
+@client.command()
+async def allmemes(ctx):
+    collection = db[str(ctx.guild)]
+
+    if collection.count_documents({}) < 1:
+        await ctx.send("You have not yet added any memes. You can add memes by using the $addmeme command")
+        return
+
+    allMemes = "Here are the memes you've added so far: \n\n"
+
+    for post in collection.find():
+        allMemes += f"${post.get('name')}\n"
+
+    await ctx.send(allMemes)
+
+
+# Downloads, trims and converts audio file from youtube video and registers name-file relationship in DB. Todo: S3 integration
 @client.command()
 async def addmeme(ctx, memeName, url, start, end):
     # Check valid argument entry
@@ -45,7 +73,7 @@ async def addmeme(ctx, memeName, url, start, end):
     
     if not start or not end:
         await ctx.send("Please enter start and end timestamps.")
-
+        return
 
     # Convert timestamps to seconds
     try:
@@ -59,6 +87,7 @@ async def addmeme(ctx, memeName, url, start, end):
             return
         if start > end:
             await ctx.send("End timestamp needs to be after starting timestamp. Try again.")
+            return
             
     except:
         print("Conversion went wrong")
@@ -66,24 +95,25 @@ async def addmeme(ctx, memeName, url, start, end):
         return
 
     # Creating DB and checking if memeName already exists
-    db = TinyDB("db.json")
-    Meme = Query()
-    memeDocList = db.search(Meme.name == memeName)
-    if memeDocList:
-        await ctx.send("memeName already exists")
+    collection = db[str(ctx.guild)]
+    if collection.find_one({"name": memeName}):
+        await ctx.send("Meme name already exists. Try again.")
         return
 
     # Create filename with unique ID
     memeID = uuid.uuid1().hex
     fileName = memeID + " - " + memeName
 
-    # If memeName is new and arguments are valid, the audio is downloaded, trimmed and saved. 
-    # memeName and filename are stored in DB.
+    # Audio is downloaded, trimmed and saved. Meme is saved in DB
     yt = YouTube(url)
     saved = yt.streams.get_audio_only().download(output_path="audiofiles/", filename=fileName)
     
+    sleptFor = 0
     while not saved:
         time.sleep(1)
+        sleptFor += 1
+        if sleptFor > 20:
+            ctx.send("Video download time expired.")
 
     print("Audio downloaded")
 
@@ -93,37 +123,38 @@ async def addmeme(ctx, memeName, url, start, end):
     ffmpeg.run(stream)
     print("Audio trimmed and converted")
 
-    db.insert({"name": memeName, "path": f"audiofiles/{fileName}.ogg"})
+    collection.insert_one({"name": memeName, "path": f"audiofiles/{fileName}.ogg"})
 
     mp4Exists = os.path.exists(f"audiofiles/{fileName}.mp4")
     oggExists = os.path.exists(f"audiofiles/{fileName}.ogg")
 
+    # Old mp4 file is removed
     if mp4Exists and oggExists:
         os.remove(f"audiofiles/{fileName}.mp4")
     else:
         print("Could not remove mp4 because it doesn't exist.")
 
-    await ctx.send("Meme added to DB. Ready for use.")
+    await ctx.send(f"\"{memeName}\" meme has been added to your collection! Use it with the command $meme {memeName}")
 
 
+# Recalls meme with chosen name argument. Todo: fetch from S3
 @client.command()
 async def meme(ctx, memeName):
-    # Searching if meme already exists
-    db = TinyDB("db.json")
-    Meme = Query()
-    memeDocList = db.search(Meme.name == memeName)
-
-    if not memeDocList:
-        await ctx.send("Meme not in DB. Use '$list' command for all memes or '$addmeme' to register a new meme.")
+    # Searching if meme exists
+    collection = db[str(ctx.guild)]
+    docToPlay = collection.find_one({"name": memeName})
+    if not docToPlay:
+        await ctx.send("This meme is not in the database. Use \"$allmemes\" command for all memes or \"$addmeme\" to register a new meme.")
         return
 
-    # If meme exists, connect to server
-    path = memeDocList[0]["path"]
+    path = docToPlay.get("path")
+
+    # Connect to voice channel
     try:
         vc = await connect_vc(ctx.message.author.voice.channel)
     except:
-        print("Could not connect or find Voice Client. Try again.")
-        pass
+        await ctx.send("Could not connect or find Voice Client. Try again.")
+        return
 
     # Function for handling errors and queueing next clip
     def afterHandler(error):
@@ -136,6 +167,7 @@ async def meme(ctx, memeName):
         
         if not qdb.all():
             print("Done with queue.")
+            return
         else:
             doc = qdb.all()[0]
 
@@ -147,14 +179,15 @@ async def meme(ctx, memeName):
 
             vc.play(discord.FFmpegPCMAudio(qpath), after=afterHandler)
 
-            
 
+    # Playing audio
     if not vc.is_playing():
         vc.play(discord.FFmpegPCMAudio(path), after=afterHandler)
     else:
         addToQueue(memeName, path)
 
 
+# Removes meme audio file and DB entry
 @client.command()
 async def remove(ctx, memeName):
     db = TinyDB("db.json")
@@ -174,6 +207,7 @@ async def remove(ctx, memeName):
     await ctx.send(f"Removed \"{memeName}\" from DB.")
 
 
+# Converts timecode format entered by user to seconds
 def convertTime(a):
     if not re.match(r"^[0-9:.]+$", a):
         return "Invalid Timecode"
@@ -221,13 +255,14 @@ def convertTime(a):
         return hrs + mins + secs
 
 
-
+# Callback function to add to-be-played memes to a playback queue
 def addToQueue(memeName, path):
     qdb = TinyDB("queue.json")
     qdb.insert({"name": memeName, "path": path})
     print(f"\"{memeName}\" added to queue")        
 
 
+# Connects bot to a voice channel
 async def connect_vc(channel): 
     # Connects to Voice Client if not already connected and returns it.
     vc_list = client.voice_clients
@@ -241,6 +276,6 @@ async def connect_vc(channel):
         if vc.is_connected():
             print(f"Already connected to Voice Client {vc}")
     return vc
+    
 
-
-client.run(token)
+client.run(discordToken)
